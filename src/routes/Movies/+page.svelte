9f2f9d51a85e3098301f46f444a2b67c1109ad0b
nicolas <nicolassauter97@gmail.com>
2024-12-10 11:54:17 +0100

    finished

--- src/routes/Movies/+page.svelte
+++ src/routes/Movies/+page.svelte
@@ -1,44 +1,46 @@
 <script>
   import ComponentFilme from "$lib/components/component_filme.svelte";
-  let {data} = $props();
-
+  let { data } = $props();
 </script>
 
-<div class="container mt-5" >
-<div class="container text-center">
-  <h1>Aktuelle Filme</h1>
-  <p><em>Daten und Bilder generiert von ChatGPT unc DALL-E</em></p>
-  <a href="/movies/create" class="btn btn-primary">Add New Movie</a>
+<div class="container mt-5">
+  <div class="container text-center">
+    <h1>Aktuelle Filme</h1>
+    <p><em>Daten und Bilder generiert von ChatGPT und DALL-E</em></p>
+    <a href="/movies/create" class="btn btn-primary">Add New Movie</a>
   </div>
 </div>
 
-
 <div class="container text-left mt-3">
-      <div class="row">
-        {#each data.movies as movie}
-          <div class="col-md-3 mb-4">
-              <div class="card bg-dark text-light shadow" >
-              <img src={movie.poster} class="card-img-top" alt={movie.title} />
-              <div class="card-body">
-                <h5>
-                  <a href={`/movies/${movie._id}`} class="card-title">{movie.title}</a>
-                </h5>
-                <p class="card-text">Jahr: {movie.year}</p>
-                <p class="card-text">Dauer: {movie.duration}</p>
-              </div>
-            </div>
+  <div class="row">
+    {#each data.movies as movie}
+      <div class="col-md-3 mb-4">
+        <div class="card bg-dark text-light shadow">
+          <img src={movie.poster} class="card-img-top" alt={movie.title} />
+          <div class="card-body">
+            <h5>
+              <a href={`/movies/${movie._id}`} class="card-title no-underline">{movie.title}</a>
+            </h5>
+            <p class="card-text">Jahr: {movie.year}</p>
+            <p class="card-text">Dauer: {movie.duration}</p>
           </div>
-        {/each}
+        </div>
       </div>
-    </div>
-   
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    max-width: 1200px;
+  }
+
+  .no-underline {
+    text-decoration: none;
+  }
 
 
-    <style>
-      .container {
-          max-width: 1200px;
-      }
-      </style>
-
-
-
+  .no-underline:hover {
+    text-decoration: underline;
+  }
+</style>
--- src/routes/Movies/create/+page.svelte
+++ src/routes/Movies/create/+page.svelte
@@ -8,35 +8,49 @@
     <h2 class="mb-4">Add a Movie</h2>
     <form method="POST" action="?/create" class="needs-validation">
         <div class="mb-3">
-            <label for="name" class="form-label">Name of the Movie</label>
+            <label for="name" class="form-label">Name:</label>
             <input
                 id="name"
                 name="name"
                 class="form-control"
                 type="text"
                 required
+                placeholder="Enter the name of the movie"
             />
         </div>
         <div class="mb-3">
-            <label for="year" class="form-label">Year where the Movie came out (Only Numbers)</label>
+            <label for="year" class="form-label">Year:</label>
             <input
                 id="year"
                 name="year"
                 class="form-control"
                 type="number"
                 required
+                placeholder="Enter the year"
             />
         </div>
         <div class="mb-3">
-            <label for="duration" class="form-label">Duration of the Movie (Only Numbers)</label>
+            <label for="duration" class="form-label">Duration:</label>
             <input
                 id="duration"
                 name="duration"
                 class="form-control"
                 type="number"
                 required
+                placeholder="Enter the duration in minutes"
             />
         </div>
+        <div class="mb-3">
+            <label for="actors" class="form-label">Actors:</label>
+            <input
+              id="actors"
+              name="actors"
+              class="form-control"
+              type="text"
+              required
+              placeholder="Enter actors, separated by commas"
+            />
+          </div>          
         <button type="submit" class="btn btn-primary">Add Movie</button>
     </form>
 </div>
