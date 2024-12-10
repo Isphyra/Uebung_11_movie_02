0dadc6c8c1135159f1a1bc2ddc05dfdf2b22b8da
nicolas <nicolassauter97@gmail.com>
2024-12-10 10:51:05 +0100

    added

--- src/lib/components/component_filme.svelte
+++ src/lib/components/component_filme.svelte
@@ -2,9 +2,9 @@
     let {movie} = $props();
 </script>
 <div>
-    <img src={movie.Poster} alt={movie.Title} />
-    <h3>{movie.Title}</h3>
+    <img src={movie.poster} alt={movie.title} />
+    <h3>{movie.title}</h3>
     <p>Year: {movie.year}</p>
-    <p>Duration: {movie.Duration}</p>
+    <p>Duration: {movie.duration}</p>
 </div>
     
--- src/routes/Movies/+page.svelte
+++ src/routes/Movies/+page.svelte
@@ -1,34 +1,38 @@
 <script>
   import ComponentFilme from "$lib/components/component_filme.svelte";
   let {data} = $props();
+
 </script>
 
 <div class="container mt-5" >
 <div class="container text-center">
   <h1>Aktuelle Filme</h1>
   <p><em>Daten und Bilder generiert von ChatGPT unc DALL-E</em></p>
-  <a href="/Movies/create" class="btn btn-primary">Add New Movie</a>
+  <a href="/movies/create" class="btn btn-primary">Add New Movie</a>
   </div>
 </div>
 
-<div class="container text-left">
+
+<div class="container text-left mt-3">
       <div class="row">
         {#each data.movies as movie}
           <div class="col-md-3 mb-4">
               <div class="card bg-dark text-light shadow" >
-              <img src={movie.poster} class="card-img-top" alt={movie.Title} />
+              <img src={movie.poster} class="card-img-top" alt={movie.title} />
               <div class="card-body">
                 <h5>
-                  <a href={`/movies/${movie._id}`} class="card-title">{movie.Title}</a>
+                  <a href={`/movies/${movie._id}`} class="card-title">{movie.title}</a>
                 </h5>
-                <p class="card-text">Jahr: {movie.Year}</p>
-                <p class="card-text">Dauer: {movie.Duration}</p>
+                <p class="card-text">Jahr: {movie.year}</p>
+                <p class="card-text">Dauer: {movie.duration}</p>
               </div>
             </div>
           </div>
         {/each}
       </div>
     </div>
+   
+
 
     <style>
       .container {
